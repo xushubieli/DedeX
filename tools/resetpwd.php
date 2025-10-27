c9a4b21d3a1c603d3a14bca703fa266fb6eb3409
叙述、别离 <93301500+xushubieli@users.noreply.github.com>
2025-10-27 13:48:22 +0800

    Update resetpwd.php

--- tools/resetpwd.php
+++ tools/resetpwd.php
@@ -17,12 +17,12 @@ define('DEDEX_REPWD_VER', '1.0.2');
  */
 function ToolAlert($content, $colors = array('#cfe2ff', '#b6d4fe', '#084298'))
 {
-    define('TOOLS_ALERT_TPL', '<div style="position:relative;padding:0.75rem 1.25rem;margin-bottom:1rem;width:auto;font-size:14px;color:~color~;background:~background~;border-color:~border~;border:1px solid transparent;border-radius:0.5rem">~content~</div>');
+    define('TOOLS_ALERT_TPL', '<div style="position:relative;padding:1rem;margin-bottom:1rem;width:auto;font-size:14px;color:~color~;background:~background~;border-color:~border~;border:1px solid transparent;border-radius:0.5rem">~content~</div>');
     list($background, $border, $color) = $colors;
     return str_replace(array('~color~', '~background~', '~border~', '~content~'), array($color, $background, $border, $content), TOOLS_ALERT_TPL);
 }
 if (!file_exists(dirname(__FILE__).'/system/common.inc.php')) {
-    echo ToolAlert("请把resetpwd.php文件放置网站根目录，通过http://域名/resetpwd.php访问操作");
+    echo ToolAlert("请把resetpwd.php文件放置网站根目录，通过：域名/resetpwd.php访问进行操作");
     exit();
 }
 require_once dirname(__FILE__).'/system/common.inc.php';
@@ -67,7 +67,7 @@ if ($dopost === 'change') {
 }
 $wintitle = "DedeX管理员密码修改工具".DEDEX_REPWD_VER;
 $win = new OxWindow();
-$win->Init(basename(__FILE__), 'js/blank.js', 'POST');
+$win->Init(basename(__FILE__));
 $win->AddHidden('dopost', 'change');
 $win->AddHidden('token', $_SESSION['token']);
 $win->AddMsgItem('<tr>
@@ -79,12 +79,12 @@ $win->AddMsgItem('<tr>
     <td><input type="password" name="newpwd" id="newpwd" class="admin-input-lg" placeholder="请输入新的管理员密码"></td>
 </tr>
 <tr>
-    <td>验证管理员新密码</td>
-    <td><input type="password" name="renewpwd" id="renewpwd" class="admin-input-lg" placeholder="请再次输入管理员密码"></td>
+    <td>管理员新密码验证</td>
+    <td><input type="password" name="renewpwd" id="renewpwd" class="admin-input-lg" placeholder="请再次输入管理员密码验证"></td>
 </tr>
 <tr>
-    <td>数据库连接密码</td>
-    <td><input type="password" name="dbpwd" id="dbpwd" class="admin-input-lg" placeholder="请输入数据库连接密码">（查看根目录/data/common.inc.php文件中的$cfg_dbpwd）</td>
+    <td>数据库密码</td>
+    <td><input type="password" name="dbpwd" id="dbpwd" class="admin-input-lg" placeholder="请输入数据库密码"></td>
 </tr>');
 $winform = $win->GetWindow('ok');
 $win->Display();
